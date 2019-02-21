a0cd09513e0c94fa6fef81bdce9dca25659a0cbe
Fisser <mfisser@sva.de>
2019-02-21 09:51:14 +0100

    change minior

--- imagebuildfile.sh
+++ imagebuildfile.sh
@@ -4,5 +4,5 @@ export version=1.0.0
 export hive_version=2.3.2
 
 cd ~/repos/masterarbeit/docker-hive-metastore-postgresql
-docker build -t mfisser/hive:${version}-hive${hadoop_version}-for-postgresql-metastore ./
-docker push mfisser/hive:${version}-hive${hadoop_version}-for-postgresql-metastore
+docker build -t mfisser/postgres:${version}-hive${hadoop_version}-for-postgresql-metastore ./
+docker push mfisser/postgres:${version}-hive${hadoop_version}-for-postgresql-metastore
